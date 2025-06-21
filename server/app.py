81197a67ace34b0c5b1f38986b3bae3d1781e699
keithkipkoech <keithselim@gmail.com>
2025-06-22 01:06:13 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask import Flask, request, make_response
 from flask_restful import Api, Resource
 import os
+from flask_cors import CORS
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
@@ -90,6 +91,7 @@ class RestaurantPizzas(Resource):
 api.add_resource(RestaurantPizzas, "/restaurant_pizzas")
 
 
+CORS(app)
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
